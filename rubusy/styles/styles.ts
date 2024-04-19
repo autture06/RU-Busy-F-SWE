4c06cd2764739cd03296f13633d479a0f73ab688
autture06 <arya.utture@gmail.com>
2024-04-19 18:52:27 -0400

    styles file updated

---
+++ rubusy/styles/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native/types";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+    },
+
+    button: {
+        marginBottom: 10,
+    },
+});
