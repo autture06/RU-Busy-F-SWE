1ec1817d2b348cbac031826a53c98be930dd0adc
autture06 <arya.utture@gmail.com>
2024-04-20 02:44:55 -0400

    Login file updated

--- rubusy/screens/Login.tsx
+++ rubusy/screens/Login.tsx
@@ -1,26 +1,76 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
+//formik
+import { Formik } from 'formik';
+
+//icons
+import { Octicons } from '@expo/vector-icons';
+
 import {
     StyledContainer,
     InnerContainer,
     PageLogo,
     PageTitle,
     SubTitle,
-    StyledFormArea
-} from '../components/styles'
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    Colors
+} from '../components/styles';
+import { View } from 'react-native';
+
+// Define MyTextInput before using it
+const MyTextInput = ({ label, icon, ...props }) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={red} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+        </View>
+    );
+};
+
+//Colors
+const { brand, darkLight, red } = Colors;
 
 const Login = () => {
     return (
         <StyledContainer>
-            <StatusBar style = 'dark'/>
+            <StatusBar style='dark' />
             <InnerContainer>
-                <PageLogo resizeMode = "cover" source = {require('./../assets/images/logo.jpg')}/>
+                <PageLogo resizeMode="cover" source={require('./../assets/images/logo.jpg')} />
                 <PageTitle>RU Busy</PageTitle>
                 <SubTitle>Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <StyledFormArea>
+                            <MyTextInput
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="first.last@gmail.com"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                                keyboardType="email-address"
+                            />
+                        </StyledFormArea>
+                    )}
+                </Formik>
             </InnerContainer>
         </StyledContainer>
     );
-}
+};
 
 export default Login;
