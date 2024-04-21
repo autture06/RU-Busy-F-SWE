becc9ccd82c5311e4f3fee7613a39cb8164b33dc
autture06 <arya.utture@gmail.com>
2024-04-21 17:20:03 -0400

    Welcome file update

--- rubusy/screens/Welcome.jsx
+++ rubusy/screens/Welcome.jsx
@@ -1,66 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-//formik
-import { Formik } from 'formik';
-
-//icons
-import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
-
 import {
-    StyledContainer,
     InnerContainer,
-    PageLogo,
     PageTitle,
     SubTitle,
     StyledFormArea,
-    LeftIcon,
-    StyledInputLabel,
-    StyledTextInput,
-    RightIcon,
     StyledButton,
     ButtonText,
-    Colors,
-    MsgBox,
     Line,
-    ExtraView,
-    ExtraText,
-    TextLink,
-    TextLinkContent,
     WelcomeContainer,
     WelcomeImage,
     Avatar
 } from '../components/styles';
-import { View } from 'react-native';
-
-const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
-    return (
-        <View>
-            <LeftIcon>
-                <Octicons name={icon} size={30} color={red} />
-            </LeftIcon>
-            <StyledInputLabel>{label}</StyledInputLabel>
-            <StyledTextInput {...props} />
-            {isPassword && (
-                <RightIcon onPress = {() => setHidePassword(!hidePassword)}>
-                    <Ionicons name = {hidePassword ? 'eye-off' : 'eye'} size = {30} color = {darkLight}/>
-                </RightIcon>
-            )}
-        </View>
-    );
-};
-
-//Colors
-const { darkLight, red, primary } = Colors;
 
 const Welcome = () => {
-    const [hidePassword, setHidePassword] = useState(true);
-
     return (
         <>
-            <StatusBar style='dark' />
+            <StatusBar style="light" />
             <InnerContainer>
-                <WelcomeImage resizeMode="cover" source={require('./../assets/images/logo.jpg')} />
+                <WelcomeImage resizeMode="cover" source={require('./../assets/images/welcImg.jpg')} />
                 <WelcomeContainer>
                     <PageTitle welcome = {true}>Welcome!!!</PageTitle>
                     <SubTitle welcome = {true}>First and Last Name</SubTitle>
