fc3338a51052105fc6574ca9491b77e2a3276bb1
autture06 <arya.utture@gmail.com>
2024-04-21 01:38:15 -0400

    styles file updated

--- rubusy/components/styles.jsx
+++ rubusy/components/styles.jsx
@@ -90,8 +90,9 @@ export const RightIcon = styled.TouchableOpacity`
 
 export const StyledButton = styled.TouchableOpacity`
     padding: 15px;
-    bachground-color: ${brand};
+    background-color: ${red};
     justify-content: center;
+    align-items: center;
     border-radius: 5px;
     margin-vertical: 5px;
     height: 60px;
@@ -100,4 +101,16 @@ export const StyledButton = styled.TouchableOpacity`
 export const ButtonText = styled.Text`
     color: ${primary};
     font-size: 16px;
+`;
+
+export const MsgBox = styled.Text`
+    text-align: center;
+    font-size: 13px;
+`;
+
+export const Line = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${darkLight};
+    margin-vertical: 10px;
 `;
