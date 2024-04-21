64079d1a550602ec6055689d1d6652961d2b4093
autture06 <arya.utture@gmail.com>
2024-04-21 17:25:30 -0400

    Welcome file updated

--- rubusy/screens/Welcome.jsx
+++ rubusy/screens/Welcome.jsx
@@ -1,40 +1,118 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 
+//formik
+import { Formik } from 'formik';
+
+//icons
+import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
+
 import {
+    StyledContainer,
     InnerContainer,
+    PageLogo,
     PageTitle,
     SubTitle,
     StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
     StyledButton,
     ButtonText,
+    Colors,
+    MsgBox,
     Line,
-    WelcomeContainer,
-    WelcomeImage,
-    Avatar
+    ExtraView,
+    ExtraText,
+    TextLink,
+    TextLinkContent
 } from '../components/styles';
+import { View } from 'react-native';
+
+//Colors
+const { darkLight, red, primary } = Colors;
 
 const Welcome = () => {
+    const [hidePassword, setHidePassword] = useState(true);
+
     return (
-        <>
-            <StatusBar style="light" />
+        <StyledContainer>
+            <StatusBar style='dark' />
             <InnerContainer>
-                <WelcomeImage resizeMode="cover" source={require('./../assets/images/welcImg.jpg')} />
-                <WelcomeContainer>
-                    <PageTitle welcome = {true}>Welcome!!!</PageTitle>
-                    <SubTitle welcome = {true}>First and Last Name</SubTitle>
-                    <SubTitle welcome = {true}>first.last@gmail.com</SubTitle>
+                <PageLogo resizeMode="cover" source={require('./../assets/images/logo.jpg')} />
+                <PageTitle>RU Busy</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
                         <StyledFormArea>
-                            <Avatar resizeMode="cover" source={require('./../assets/images/logo.jpg')} />
-                        <Line />
-                            <StyledButton onPress = {() => {}}>
-                                <ButtonText>Logout</ButtonText>
+                            <MyTextInput
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="first.last@gmail.com"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                                keyboardType="email-address"
+                            />
+
+                            <MyTextInput
+                                label="Password"
+                                icon="lock"
+                                placeholder="********"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                                secureTextEntry = {hidePassword}
+                                isPassword = {true}
+                                hidePassword = {hidePassword}
+                                setHidePassword = {setHidePassword}
+                            />
+                            <MsgBox>...</MsgBox>
+                            <StyledButton onPress = {handleSubmit}>
+                                <ButtonText>Login</ButtonText>
                             </StyledButton>
-                            
+                            <Line />
+                            <StyledButton google = {true} onPress = {handleSubmit}>
+                                <Fontisto name = "google" color = {primary} size = {25}/>
+                                <ButtonText google = {true}>Sign in with Google</ButtonText>
+                            </StyledButton>
+                            <ExtraView>
+                                <ExtraText>Don't have an account already? </ExtraText>
+                                <TextLink>
+                                    <TextLinkContent>Signup</TextLinkContent>
+                                </TextLink>
+                            </ExtraView>
                         </StyledFormArea>
-                </WelcomeContainer>
+                    )}
+                </Formik>
             </InnerContainer>
-        </>
+        </StyledContainer>
+    );
+};
+
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={red} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+            {isPassword && (
+                <RightIcon onPress = {() => setHidePassword(!hidePassword)}>
+                    <Ionicons name = {hidePassword ? 'eye-off' : 'eye'} size = {30} color = {darkLight}/>
+                </RightIcon>
+            )}
+        </View>
     );
 };
 
