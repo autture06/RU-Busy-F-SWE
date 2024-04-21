a9044e290bccb7ba41c850c1891fd5e0f632c698
autture06 <arya.utture@gmail.com>
2024-04-21 18:12:06 -0400

    Welcome file updated

--- rubusy/screens/Welcome.jsx
+++ rubusy/screens/Welcome.jsx
@@ -1,43 +1,20 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-//formik
-import { Formik } from 'formik';
-
-//icons
-import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
-
 import {
-    StyledContainer,
     InnerContainer,
-    PageLogo,
     PageTitle,
     SubTitle,
     StyledFormArea,
-    LeftIcon,
-    StyledInputLabel,
-    StyledTextInput,
-    RightIcon,
     StyledButton,
     ButtonText,
-    Colors,
-    MsgBox,
     Line,
-    ExtraView,
-    ExtraText,
-    TextLink,
-    TextLinkContent,
     WelcomeContainer, 
     WelcomeImage, 
     Avatar
 } from '../components/styles';
-import { View } from 'react-native';
-
-//Colors
-const { darkLight, red, primary } = Colors;
 
 const Welcome = () => {
-    const [hidePassword, setHidePassword] = useState(true);
 
     return (
         <>
@@ -58,23 +35,6 @@ const Welcome = () => {
                 </WelcomeContainer>
             </InnerContainer>
         </>
-    );
-};
-
-const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
-    return (
-        <View>
-            <LeftIcon>
-                <Octicons name={icon} size={30} color={red} />
-            </LeftIcon>
-            <StyledInputLabel>{label}</StyledInputLabel>
-            <StyledTextInput {...props} />
-            {isPassword && (
-                <RightIcon onPress = {() => setHidePassword(!hidePassword)}>
-                    <Ionicons name = {hidePassword ? 'eye-off' : 'eye'} size = {30} color = {darkLight}/>
-                </RightIcon>
-            )}
-        </View>
     );
 };
 
