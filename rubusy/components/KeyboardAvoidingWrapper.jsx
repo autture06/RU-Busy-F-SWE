4d729ba4c737e32fddcfdd57ea651668280ab15f
autture06 <arya.utture@gmail.com>
2024-04-22 02:19:52 -0400

    keyboard file updated

--- rubusy/components/KeyboardAvoidingWrapper.jsx
+++ rubusy/components/KeyboardAvoidingWrapper.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 //keyboard avoiding view
-import {KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import {KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard, Platform} from 'react-native';
 
 const KeyboardAvoidingWrapper = ({children}) => {
     return (
-        <KeyboardAvoidingView style = {{flex: 1}}>
-            <ScrollView>
+        <KeyboardAvoidingView style = {{flex: 1}} behavior="padding" keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : -500}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
                 <TouchableWithoutFeedback onPress = {Keyboard.dismiss}>
                     {children}
                 </TouchableWithoutFeedback>
