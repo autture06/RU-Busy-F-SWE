916876e5031f499cb83021d5295f00643c22d52e
autture06 <arya.utture@gmail.com>
2024-04-22 02:04:36 -0400

    Login file updated

--- rubusy/screens/Login.jsx
+++ rubusy/screens/Login.jsx
@@ -33,69 +33,74 @@ import { View } from 'react-native';
 //Colors
 const { darkLight, red, primary } = Colors;
 
+//keyboard avoiding view
+import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
+
 const Login = () => {
     const [hidePassword, setHidePassword] = useState(true);
 
     return (
-        <StyledContainer>
-            <StatusBar style='dark' />
-            <InnerContainer>
-                <PageLogo resizeMode="cover" source={require('./../assets/images/logo.jpg')} />
-                <PageTitle>RU Busy</PageTitle>
-                <SubTitle>Account Login</SubTitle>
+        <KeyboardAvoidingWrapper>
+            <StyledContainer>
+                <StatusBar style='dark' />
+                <InnerContainer>
+                    <PageLogo resizeMode="cover" source={require('./../assets/images/logo.jpg')} />
+                    <PageTitle>RU Busy</PageTitle>
+                    <SubTitle>Account Login</SubTitle>
 
-                <Formik
-                    initialValues={{ email: '', password: '' }}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                >
-                    {({ handleChange, handleBlur, handleSubmit, values }) => (
-                        <StyledFormArea>
-                            <MyTextInput
-                                label="Email Address"
-                                icon="mail"
-                                placeholder="first.last@gmail.com"
-                                placeholderTextColor={darkLight}
-                                onChangeText={handleChange('email')}
-                                onBlur={handleBlur('email')}
-                                value={values.email}
-                                keyboardType="email-address"
-                            />
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+                        onSubmit={(values) => {
+                            console.log(values);
+                        }}
+                    >
+                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+                            <StyledFormArea>
+                                <MyTextInput
+                                    label="Email Address"
+                                    icon="mail"
+                                    placeholder="first.last@gmail.com"
+                                    placeholderTextColor={darkLight}
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    value={values.email}
+                                    keyboardType="email-address"
+                                />
 
-                            <MyTextInput
-                                label="Password"
-                                icon="lock"
-                                placeholder="********"
-                                placeholderTextColor={darkLight}
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                value={values.password}
-                                secureTextEntry = {hidePassword}
-                                isPassword = {true}
-                                hidePassword = {hidePassword}
-                                setHidePassword = {setHidePassword}
-                            />
-                            <MsgBox>...</MsgBox>
-                            <StyledButton onPress = {handleSubmit}>
-                                <ButtonText>Login</ButtonText>
-                            </StyledButton>
-                            <Line />
-                            <StyledButton google = {true} onPress = {handleSubmit}>
-                                <Fontisto name = "google" color = {primary} size = {25}/>
-                                <ButtonText google = {true}>Sign in with Google</ButtonText>
-                            </StyledButton>
-                            <ExtraView>
-                                <ExtraText>Don't have an account already? </ExtraText>
-                                <TextLink>
-                                    <TextLinkContent>Signup</TextLinkContent>
-                                </TextLink>
-                            </ExtraView>
-                        </StyledFormArea>
-                    )}
-                </Formik>
-            </InnerContainer>
-        </StyledContainer>
+                                <MyTextInput
+                                    label="Password"
+                                    icon="lock"
+                                    placeholder="********"
+                                    placeholderTextColor={darkLight}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    value={values.password}
+                                    secureTextEntry = {hidePassword}
+                                    isPassword = {true}
+                                    hidePassword = {hidePassword}
+                                    setHidePassword = {setHidePassword}
+                                />
+                                <MsgBox>...</MsgBox>
+                                <StyledButton onPress = {handleSubmit}>
+                                    <ButtonText>Login</ButtonText>
+                                </StyledButton>
+                                <Line />
+                                <StyledButton google = {true} onPress = {handleSubmit}>
+                                    <Fontisto name = "google" color = {primary} size = {25}/>
+                                    <ButtonText google = {true}>Sign in with Google</ButtonText>
+                                </StyledButton>
+                                <ExtraView>
+                                    <ExtraText>Don't have an account already? </ExtraText>
+                                    <TextLink>
+                                        <TextLinkContent>Signup</TextLinkContent>
+                                    </TextLink>
+                                </ExtraView>
+                            </StyledFormArea>
+                        )}
+                    </Formik>
+                </InnerContainer>
+            </StyledContainer>
+        </KeyboardAvoidingWrapper>
     );
 };
 
