defb1961fd4d7e36b27eb2503a7dc721b09eb621
autture06 <arya.utture@gmail.com>
2024-04-22 19:15:54 -0400

    App file updated

--- rubusy/App.jsx
+++ rubusy/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {useAuth0, Auth0Provider} from 'react-native-auth0';
 
 //React navigation stack
 import RootStack from './navigators/RootStack';
 
 export default function App() {
   return (
-    <Auth0Provider domain={"{dev-ugmyflf2idkyptub.us.auth0.com}"} clientId={"{eMjEgT5C3h83wf9tArSoI1k2ZROcDuUb}"}>
       <RootStack />
-    </Auth0Provider>
   )
 }
