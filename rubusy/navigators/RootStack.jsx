f38776a1d851eb4a255fb6db903c50ef020fc80f
autture06 <arya.utture@gmail.com>
2024-04-22 02:39:37 -0400

    root stack file updated

--- rubusy/navigators/RootStack.jsx
+++ rubusy/navigators/RootStack.jsx
@@ -5,9 +5,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //screens
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Welcome from './screens/Welcome';
+import Login from './../screens/Login';
+import Signup from './../screens/Signup';
+import Welcome from './../screens/Welcome';
 
 const Stack = createNativeStackNavigator();
 
